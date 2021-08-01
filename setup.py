fe5660bf402740188d18c10e8d05d4155dd509fa
unknown <lendale.vijaylaxmi@gmail.com>
2021-08-01 14:59:31 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It is end-end implementation of wafer3 project with MLOPS',
+    author='Vijaylaxmi Lendale',
+    license='MIT',
+)
